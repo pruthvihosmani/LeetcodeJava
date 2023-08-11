360d12b39649ead3a9e10b6c581a57989fc6a442
pruthvihosmani <67551298+pruthvihosmani@users.noreply.github.com>
2023-08-11 08:24:39 +0530

    If condition synatx is written with example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+         /*
+         syntax of if statements:
+         if(boolean Expression T or F)
+         {
+         // body
+         } else {
+         // do this
+         }
+          */
+        int salary = 25000;
+        if (salary > 10000){
+            salary = salary + 2000;
+        } else {
+            salary = salary + 1000;
+        }
+        System.out.println(salary);
     }
 }
